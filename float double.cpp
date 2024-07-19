65ce7bfd0ef45953e7c44fe62d3b5d5cfb62aed0
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-07-19 23:01:20 +0530

    Create float double.cpp
    I leant about floats and doubles

---
+++ float double.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main (){
+//	float d = 34.4f;
+//	long double e = 34.4l;
+//	cout<<"The size of 34.4 is "<<sizeof(34.4)<<'\n';
+//	cout<<"The size of 34.4f is "<<sizeof(34.4f)<<'\n';
+//	cout<<"The size of 34.4F is "<<sizeof(34.4F)<<'\n';
+//	cout<<"The size of 34.4l is "<<sizeof(34.4l)<<'\n';
+//	cout<<"The size of 34.4L is "<<sizeof(34.4L)<<'\n'
+
+
+
+//    float c = 44.55f;
+//    float & d = c;
+//    cout<<d<<'\n';
+//    cout<<c<<'\n';
+
+    float e = 444.55f;
+    int w = 555;
+    cout<<"The int e is :"<<(int)e<<'\n';
+    cout<<"The float w is :"<<(int)w<<'\n';
+    cout<<"The e + w  is :"<<e+w<<'\n';
+    cout<<"The int e + w is :"<<(int)e +w<<'\n';
+	return 0;
+}
