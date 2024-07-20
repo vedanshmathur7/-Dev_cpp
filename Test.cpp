57c5e5527ce6af672fb5e9458e39342ede89eaca
Vedansh Mathur <vedanshmathur007@gmail.com>
2024-07-20 16:20:27 +0530

    Date 20/07/24 learnings

---
+++ Test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main() {
+    int age;
+    cout<<"enter your age my boy!"<<endl;
+    cin>>age;
+    if(age>=18){
+        cout<<"You are matured one! "<<endl;
+    }
+    else{
+        cout<<"YOu are a kid my boy! "<<endl;
+    }
+    cout<<"Hope you had a great time .. huhh!!"<<endl;
+    return 0;
+ }
